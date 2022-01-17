dba283c683ef9a18abc40b9c0365d268d9084c62
Joon1313 <wkdrud13@gmail.com>
2022-01-17 17:59:37 +0900

    header footer

--- layouts/footer.js
+++ layouts/footer.js
@@ -5,9 +5,10 @@ import Notion from "../components/icon/notion";
 export default function Footer() {
   return (
     <footer id="footer">
-      <a href="https://github.com/Joon1313" target="_blank" rel="noreferrer">
+      <span>© 2022 Camon All Rights Reserved.</span>
+      {/* <a href="https://github.com/Joon1313" target="_blank" rel="noreferrer">
         <GitHub style={{ fontSize: "26px" }} alt="github" />
-      </a>
+      </a> */}
       {/* <a
         href="https://volcano-quill-93c.notion.site/ab9725aa81cb4f409c9066672d0baec2"
         target="_blank"
--- layouts/header.js
+++ layouts/header.js
@@ -1,11 +1,21 @@
 import { Container } from "@mui/material";
 import Link from "next/link";
+import { GitHub } from "@mui/icons-material";
 
 export default function Header() {
   return (
     <header id="header">
-      <Container maxWidth="lg">
+      <Container maxWidth="lg" id="header_box" component="nav">
         <Link href="/">Camlog</Link>
+        <div id="nav">
+          <a
+            target="_blank"
+            href="https://github.com/Joon1313"
+            rel="noopener noreferrer"
+          >
+            <GitHub style={{ fontSize: "28px" }} alt="github" />
+          </a>
+        </div>
       </Container>
     </header>
   );
