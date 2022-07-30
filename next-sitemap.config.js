52a0b3f116c24c0d8fbbd928d87b587d6181836e
Joon1313 <wkdrud13@gmail.com>
2022-07-30 13:43:27 +0900

    [style, chore] 마크다운 css 변경, gitignore 수정

--- next-sitemap.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  siteUrl: "https://camlog.vercel.app/",
-  generateRobotsTxt: true, // default: false, true 라고 설정해야 robots.txt 생성
-  sitemapSize: 7000,
-  exclude: ["/posts/write", "/login", "/posts/*/edit"],
-};
