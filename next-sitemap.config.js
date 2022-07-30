f5ca222f243e359165c875a51e49947b81f9bb84
Joon1313 <wkdrud13@gmail.com>
2022-07-30 19:36:27 +0900

    [chore] config file 추가

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: "https://camlog.vercel.app/",
+  generateRobotsTxt: true, // default: false, true 라고 설정해야 robots.txt 생성
+  sitemapSize: 7000,
+  exclude: ["/posts/write", "/login", "/posts/*/edit"],
+};
