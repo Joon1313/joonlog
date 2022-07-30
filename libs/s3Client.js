05853bc71f694842e67bb7ea283146d7a7add379
Joon1313 <wkdrud13@gmail.com>
2022-07-30 22:15:22 +0900

    [chore,style] s3 upload 에러 수정, toast 뷰어 스타일 수정

--- libs/s3Client.js
+++ libs/s3Client.js
@@ -2,7 +2,6 @@ import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
   region: process.env.REGION,
-  region: "ap-northeast-2",
   credentials: {
     accessKeyId: process.env.ACCESS_KEY,
     secretAccessKey: process.env.SECRET_KEY,
--- libs/s3Common.js
+++ libs/s3Common.js
@@ -1,8 +1,6 @@
 import { PutObjectCommand } from "@aws-sdk/client-s3";
 import { s3 } from "./s3Client";
 
-const S3_URL = "https://camlogs3.s3.ap-northeast-2.amazonaws.com/";
-
 const makeParams = (blob, title) => {
   const param = {
     Bucket: process.env.BUCKET,
@@ -12,10 +10,11 @@ const makeParams = (blob, title) => {
   };
   return param;
 };
+
 const uploadImage = async (param) => {
   try {
     await s3.send(new PutObjectCommand(param));
-    const location = S3_URL + param.Key;
+    const location = process.env.S3_URL + param.Key;
     return location;
   } catch (err) {
     console.log("Error", err);
