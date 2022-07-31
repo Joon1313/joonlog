1a08f9bd3512bd2817068fda8f0bb8f5c87b3c88
Joon1313 <wkdrud13@gmail.com>
2022-07-31 12:26:15 +0900

    [edit] formidable multiple true

--- pages/api/s3.js
+++ pages/api/s3.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       const data = await new Promise((resolve, reject) => {
-        const form = formidable();
+        const form = formidable({ multiples: true });
         form.parse(req, (err, fields, files) => {
           if (err) {
             reject({ err });
