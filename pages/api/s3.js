1200ff3b899ef88c5a3536245975e9106f127315
Joon1313 <wkdrud13@gmail.com>
2022-07-31 11:50:41 +0900

    [edit] s3 upload content-type 수정

--- pages/api/s3.js
+++ pages/api/s3.js
@@ -34,7 +34,7 @@ export default async function handler(req, res) {
         Bucket: process.env.BUCKET,
         Key: uploadPath,
         Body: buffer,
-        ContentType: data.files.blob.mi,
+        ContentType: data.files.blob.mimetype,
       };
       try {
         await s3.send(new PutObjectCommand(param));
