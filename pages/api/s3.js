8348e230f189bf2e412b6c6aeb9f208988f1063c
Joon1313 <wkdrud13@gmail.com>
2022-07-31 12:38:36 +0900

    [edit] formidable 수정

--- pages/api/s3.js
+++ pages/api/s3.js
@@ -18,7 +18,7 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       const data = await new Promise((resolve, reject) => {
-        const form = formidable({ multiples: true });
+        const form = formidable.IncomingForm({ multiples: true });
         form.parse(req, (err, fields, files) => {
           if (err) {
             reject({ err });
