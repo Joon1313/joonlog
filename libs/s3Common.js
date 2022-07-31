2bfb26caa4762dbfa2287e34da1f95904c662902
Joon1313 <wkdrud13@gmail.com>
2022-07-31 11:27:14 +0900

    [edit] fetch response 로직 변경

--- libs/s3Common.js
+++ libs/s3Common.js
@@ -3,11 +3,12 @@ const uploadImage = async (blob, title) => {
     const formData = new FormData();
     formData.append("blob", blob);
     formData.append("title", title);
-    const response = await fetch("api/s3", {
+    const response = await fetch("https://camlog.vercel.app/api/s3", {
       method: "POST",
       body: formData,
     });
-    return response.body.location;
+    const { location } = await response.json();
+    return location;
   } catch (err) {
     console.log("aws s3 error", err);
   }
