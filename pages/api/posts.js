c41b8bc0d67b8af9963c7cc18c4fddcf0987ddb5
Joon1313 <wkdrud13@gmail.com>
2022-09-11 16:32:35 +0900

    [edit] posts API 수정

--- pages/api/posts.js
+++ pages/api/posts.js
@@ -2,10 +2,10 @@ import { getLoginSession } from "../../libs/auth";
 import { createPost, updatePost } from "../../libs/post";
 
 export default async function handler(req, res) {
-  const { title, content, tag, preview, slug } = req.body;
-
+  const postData = { ...req.body };
+  const { title, content, tag, preview, slug } = postData;
+  postData.preview = postData.preview.replaceAll("*", "");
   if (!title || !content || !tag || !preview || !slug) return res.status(400).json({ message: "params is empty" });
-
   try {
     await getLoginSession(req);
   } catch (err) {
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
   switch (req.method) {
     case "POST":
       try {
-        const post = await createPost(req.body);
+        const post = await createPost(postData);
         await fetch(process.env.VERCEL_WEBHOOK);
         res.status(200).json({ message: "success", response: post });
       } catch (err) {
@@ -25,7 +25,8 @@ export default async function handler(req, res) {
       break;
     case "PUT":
       try {
-        const post = await updatePost(req.body);
+        const post = await updatePost(postData);
+        await fetch(process.env.VERCEL_WEBHOOK);
         res.status(200).json({ message: "success", response: post });
       } catch (err) {
         res.status(400).json(err);
